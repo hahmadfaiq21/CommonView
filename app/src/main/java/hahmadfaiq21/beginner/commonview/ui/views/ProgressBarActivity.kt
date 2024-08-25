cd18dac0b4d96bb03237947b47c91a01feeebba5
Husain Ahmad Faiq <hahmadfaiq@gmail.com>
2024-08-25 11:42:40 +0700

    Cleaning code

--- app/src/main/java/hahmadfaiq21/beginner/commonview/ui/views/ProgressBarActivity.kt
+++ app/src/main/java/hahmadfaiq21/beginner/commonview/ui/views/ProgressBarActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import hahmadfaiq21.beginner.commonview.data.ItemViews
 import hahmadfaiq21.beginner.commonview.databinding.ActivityProgressBarBinding
-import hahmadfaiq21.beginner.commonview.ui.views.SeekBarActivity.Companion
 
 class ProgressBarActivity : AppCompatActivity() {
 
